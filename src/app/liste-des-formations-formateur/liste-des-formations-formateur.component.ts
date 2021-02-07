7e90983661eecdc07a58d0439798e9c5551cea57
cyrine <sirin.mnassri.mohsen.lazher@gmail.com>
2021-02-07 17:34:47 +0100

    first commit

---
+++ src/app/liste-des-formations-formateur/liste-des-formations-formateur.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../task.service';
+import { Formation } from 'src/models/Formation.model';
+import { Router } from '@angular/router';
+import { Formateur } from 'src/models/Formateur.model';
+
+@Component({
+  selector: 'app-liste-des-formations-formateur',
+  templateUrl: './liste-des-formations-formateur.component.html',
+  styleUrls: ['./liste-des-formations-formateur.component.css'],
+  providers:[TaskService]
+})
+export class ListeDesFormationsFormateurComponent implements OnInit {
+
+  constructor(private taskService:TaskService,private router:Router) { }
+
+  ngOnInit() {
+    if(localStorage.getItem('token')==null)
+    {
+ alert("merci de connecter.");
+ this.router.navigate(['/Login_Formateur']);
+    }
+    else if (localStorage.getItem('usertype')!="Formateur")
+    {
+     
+   alert("vous n'êtes pas un candidat.");
+   this.router.navigate(['/Login_Formateur']);
+      
+    }
+    this.taskService.getIdTokenF(localStorage.getItem("token")).subscribe((res)=>
+    {
+
+      this.taskService.ListeDesFormationFormateur(res.toString()).subscribe((resultat)=>
+      {
+        this.taskService.sessions=resultat as Formation[];
+
+      });
+      this.taskService.getFormateurParID(res.toString()).subscribe(rst=>{
+        this.taskService.selectedFormateur=rst as Formateur;
+      })
+    })
+   
+
+  }
+
+}
